f70d18b3dc92295b1a00b431052466cb1652c49f
kn8@theecodepoet.dev <0x0sec@gmail.com>
2023-05-16 09:48:05 -0400

    update

--- scripts/index.js
+++ scripts/index.js
@@ -29,5 +29,3 @@ const initialCards = [
       link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
     },
   ];
-
-console.log("test");
--- vendor/scripts/index.js
+++ vendor/scripts/index.js
@@ -30,4 +30,3 @@ const initialCards = [
     },
   ];
 
-console.log("test");

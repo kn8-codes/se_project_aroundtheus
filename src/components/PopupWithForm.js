e12ce110d213629598bd76eda6738dedb0db9a0f
kn8@theecodepoet.dev <0x0sec@gmail.com>
2023-08-23 22:23:56 -0400

    update

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -4,7 +4,7 @@ export default class PopupWithForm extends Popup {
     constructor(popupSelector, handleFormSubmit){
       super({popupSelector});
       this.handleFormSubmit = handleFormSubmit;
-      this._popupForm = document.querySelector(".modal__form");
+      this._popupForm = this._popupElement.querySelector(".modal__form");
       this._inputList = this._popupForm.querySelectorAll(".modal__input");
     }
     _getInputValues() {
